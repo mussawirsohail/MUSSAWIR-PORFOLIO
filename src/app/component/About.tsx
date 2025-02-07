3c52f26c25704e4a70412d3ee56fe94d9e648d82
Mussawir Sohail <159765711+mussawirsohail@users.noreply.github.com>
2025-02-07 12:20:40 +0500

    Update About.tsx

--- src/app/component/About.tsx
+++ src/app/component/About.tsx
@@ -18,12 +18,7 @@ export default function About() {
           </div>
           <div className="md:w-1/2 md:pl-8">
             <p className="text-lg mb-6 text-gray-700 dark:text-gray-300">
-              I'm a passionate full stack developer with 5 years of experience in creating web applications. 
-              I love turning complex problems into simple, beautiful, and intuitive designs.
-            </p>
-            <p className="text-lg mb-6 text-gray-700 dark:text-gray-300">
-              My expertise spans across various technologies including React, Node.js, Python, and cloud platforms like AWS.
-              I'm always eager to learn new technologies and improve my skills.
+             Hello! I'm Mussawir Sohail Welcome to my portfolio! I'm a passionate FrontEnd web developer with a keen eye for design and functionality. I am pursuing an IT course of Generative AI, Web3 and Metaverse at Governor House Sindh. Where we are currently learning advanced technologies like Next JS. With a strong foundation in HTML, CSS, and TypeScript/Javascript, I enjoy creating beautiful, user-friendly websites. Over the past few months, I've worked on diverse projects, from building simple Calculator to static Student Management System. I specialize in responsive design, ensuring that every user has the best experience, no matter what device they're using.
             </p>
             <div className="flex justify-around text-center">
               <div>
